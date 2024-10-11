fd9bf356ccc2455abb906906675cf6b0c3d6af5e
ALI-ZAHRAM <Matinm2030@outlook.com>
2024-10-11 19:38:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
-
 clear
 echo ""
-echo -e "         \e[1;91m__\e[1;92m,-\e[1;93m////\e[1;92m, "
-echo -e "        \e[1;91m/__\e[1;92m) (\e[1;93mo\e[1;92m) ) "
-echo -e "          /.,--. \       "
-echo -e "         /,-'/.\. \      "
-echo -e "         \.  \.\ \ \     "
-echo -e "          \.  \\\ \  /   "
-echo -e "      \e[1;93m=====\e[1;91m((\e[1;93m=\e[1;91m((\e[1;92m\\e[1;93m=====.=== "
-echo -e "           \e[1;92m    \ \ \.\   "
-echo -e "                \ \ \'     "
-echo -e "                 \ \'      "
-echo -e "                  \ \     "
-echo -e "                   '-'    \e[1;97m "
+echo -e "\e[1;91m⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⣒⠒⠤⣴⣶⣦⡀"⠀⠀⠀⠀⠀⠀⠀
+echo -e "⠀⠀⠀⠀⠀⠀⡠⢎⡀⣶⣾⣲⣧⣬⣙⣻⣖⢷⡄"⠀⠀⠀⠀⠀⠀
+echo -e "⠀⠀⠀⠀⢠⠃⣤⣾⣯⡥⢴⣦⠴⡛⡻⢿⡿⢯⣿⡄⠀"⠀⠀⠀
+echo -e "⠀⠀⠀⢰⣯⣾⣿⣿⣿⣯⣭⠁⠀⠀⠈⠂⠅⢤⣿⣿⠀"⠀⠀⠀⠀
+echo -e "⠀⠀⠀⣷⡟⠀⠉⠛⠉⠁⠀⠀⠀⠀⠀⠀⠱⣬⢿⣿⡆"⠀⠀
+echo -e "⠀⠀⠀⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠌⠟⡠⢧"
+echo -e "⠀⠀⠀⠈⢿⠀⠀⠀⠀⠀⠀⠀⢀⣠⣀⡀⠀⠀⠀⠸⠿⢸"⠀⠀
+echo -e "⠀⠀⠀⠀⠈⢁⠀⠀⠀⠀⠀⡴⢛⣩⣤⡄⠀⠀⠀⠄⠀⠸"⠀⠀
+echo -e "⠀⠀⠀⠀⠀⠘⢟⣻⢿⣧⠀⠀⠟⠒⠊⠀⠀⠀⠀⠄⠀⠄"⠀⠀⠀
+echo -e "⠀⠀⠀⠀⠀⠀⠘⠉⠈⢹⠀⠀   ⡀   ⠈⠁⠀⡈⠀⠀⠀⣀"
+echo -e "⠀⠀⠀⠀ ⠀⠀⠐⡀⠀⢇⠀⢀⡠⠟⠢⠀⠀⠀⠀⠀⠀⢠⣿⣦"
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠐⡀⡼⠉⢉⣀⣠⣤⡀⠀⠀⠀⠀⣴⣿⣿⣿⣷"
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠰⠛⠉⠁⠀⠈⠀⠀⠀⣰⣿⣿⣿⣿⣿"
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿"
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣦⣀⣤⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿"
 echo ""
-echo -e "\e[1;91m [+] telegram : \e[1;92mGreaterIR"
-echo -e "\e[1;91m [+] Github: \e[1;92mALI-ZAHRAM\e[1;97m"
+echo -e "\e[1;92m [+] telegram : \e[1;91mGreaterIR"
+echo -e "\e[1;93m [+] Github: \e[1;91mALI-ZAHRAM\e[1;97m"
 echo ""
 read -p " Does you want to install Parrot Shell in Termux?(Yes/No) : " input
 
-if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
+if [[ $input == Yes  $input == yes  $input == y || $input == Y ]]; then
     clear
     cp bash.bashrc $HOME
     cd /data/data/com.termux/files/usr/etc
@@ -30,7 +32,7 @@ if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     echo -e "\e[1;91mSuccessfully Installed"
     echo -e "Restart Termux"
     exit  3
-elif [[ $input == No || $input == no || $input == n || $input == N ]]; then
+elif [[ $input == No  $input == no  $input == n || $input == N ]]; then
 exit 2
 else
 echo -e "\e[1;91mInvalid Option"
